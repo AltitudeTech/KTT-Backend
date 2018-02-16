bb217a330b565f3624f4180ec41cb79428088f70
DavyBello <bellooladipupo41@gmail.com>
2018-02-16 20:09:33 +0100

    removed images field to temporarily fix cloudinary problems in production

--- models/Gallery.js
+++ models/Gallery.js
@@ -13,8 +13,8 @@ var Gallery = new keystone.List('Gallery', {
 Gallery.add({
 	name: { type: String, required: true },
 	publishedDate: { type: Date, default: Date.now },
-	heroImage: { type: Types.CloudinaryImage },
-	images: { type: Types.CloudinaryImages },
+	/*heroImage: { type: Types.CloudinaryImage },
+	images: { type: Types.CloudinaryImages },*/
 });
 
 Gallery.register();
